24d500f3058d831654e9acd18cedbe156cf2d44c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-07-03 10:43:02 -0700

    Update note.rb

--- note.rb
+++ note.rb
@@ -6,6 +6,12 @@ subject
 context
 let
 expect
+
+Respec expectattion:
+https://github.com/rspec/rspec-expectations
+The test obj should receive a message .
+If the message is received, the expectation is satisfied. If not, the example fails.
+
 expect(obj).not_to receive(:method).with(value)
 expect(obj).to     receive(:method).with(value)
 
