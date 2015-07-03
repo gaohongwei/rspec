ad45772decf2cea378bb09c3e585f4e843b1f313
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-07-03 11:05:15 -0700

    Update note.rb

--- note.rb
+++ note.rb
@@ -9,6 +9,7 @@ expect
 
 Respec expectattion:
 https://github.com/rspec/rspec-expectations
+https://github.com/rspec/rspec-mocks
 The test obj should receive a message .
 If the message is received, the expectation is satisfied. If not, the example fails.
 
