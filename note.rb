c9c8eee5b5d45537618cec93484d398ff44efad2
kevin-wei-pan <kwei@paloaltonetworks.com>
2015-12-14 14:29:44 -0800

    Update note.rb

--- note.rb
+++ note.rb
@@ -50,5 +50,6 @@ expect(rendered).to have_tag('a', text: 'CSV', with:
 expect(rendered).to have_tag(:img, with: { src: '/assets/custom.png' })
 
 ##  assign value
-    allow(Settings).to receive(:office365_active) { nil }
+Settings.office_active
+    allow(Settings).to receive(:office_active) { nil }
     assign(:welcome, false)
