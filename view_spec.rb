4f9bf112047e4a00d7b900846f5651c6a9393350
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-04-27 11:46:32 -0700

    Update view_spec.rb

--- view_spec.rb
+++ view_spec.rb
@@ -1,2 +1,7 @@
 
 allow(Event).to receive(:default_per_page) { 100 }
+
+# check nested dom
+  expect(rendered).to have_tag('th', text: 'Date', with: { class: 'width_90px sortable' }) do
+    with_tag 'a', 'data-remote' => 'true', href: events_path(search: search.sort_by(:cloud_created_at).to_params)
+  end
