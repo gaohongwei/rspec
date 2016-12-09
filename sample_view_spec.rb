39e4cab6fdabd58c0eebdc3706896c9ad5c6424b
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-09 14:21:00 -0800

    Update sample_view_spec.rb

--- sample_view_spec.rb
+++ sample_view_spec.rb
@@ -2,6 +2,7 @@ find . -name "*.*"|xargs grep "expect(rendered).to "|sed 's/.*expect(rendered)/e
 
 expect(rendered).to
 expect(rendered).to .. do
+  http://www.rubydoc.info/github/kucaahbe/rspec-html-matchers/RSpec/HtmlMatchers
 expect(rendered).to_not
 expect(rendered).not_to
 
@@ -25,3 +26,17 @@ expect(rendered).to have_tag('label', text: 'Reply-to Email', with: { for: 'glob
 expect(rendered).to have_tag('li', text: 'Sort by Date', with: { field_name: 'detected_at', sort_type: 'desc', name: 'Sort by Date' })
 expect(rendered).to have_tag('th', text: 'Has a risk?', with: { class: 'text-center' })
 expect(rendered).to have_tag(:a, text: 'Advanced', with: { href: advanced_search_assets_path, class: 'btn btn-primary' })
+expect(rendered).to have_tag('textarea', text: "I like pie", with: {:name => 'user[description]'}, )
+  
+expect(rendered).to have_tag('.user_role') do
+  with_radio_button('user[role]', 'limited_admin')
+  with_radio_button('user[role]', 'admin')
+  with_radio_button('user[role]', 'super_admin')
+  with_radio_button('user[role]', 'read_only')
+end
+expect(rendered).to have_tag(:h4, text: title, with: { class: 'filter-name' }) do
+  with_tag 'img', with: { class: 'chevron' }
+end      
+expect(rendered).to have_tag(:div, id: 'overview-tab') do
+  with_tag('h2', text: /Overview/)
+end 
