3d164ba0c630bddf8ad1eb3a8d38aec49c88271d
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-17 19:45:11 -0800

    Update model_spec.rb

--- model_spec.rb
+++ model_spec.rb
@@ -6,3 +6,5 @@ expect(person).to have_attributes(:name => "Jim", :age => 32)
 expect(person).to have_attributes(:name => a_string_starting_with("J"), :age => (a_value > 30) )
 
 expect(obj).to respond_to(:foo) # pass if obj.respond_to?(:foo)
+
+expect(obj.vertices).to match_array(array_without_order)
