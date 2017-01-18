1a8edc7749fc82a0ece8d08ba1c008c3335e0aad
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-17 16:35:09 -0800

    Update model_spec.rb

--- model_spec.rb
+++ model_spec.rb
@@ -4,3 +4,5 @@ person = Person.new("Jim", 32)
 
 expect(person).to have_attributes(:name => "Jim", :age => 32)
 expect(person).to have_attributes(:name => a_string_starting_with("J"), :age => (a_value > 30) )
+
+expect(obj).to respond_to(:foo) # pass if obj.respond_to?(:foo)
