997e25dd9438cc44e10b5084ae784f2d87b04cfc
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-23 16:28:46 -0800

    Update model_spec.rb

--- model_spec.rb
+++ model_spec.rb
@@ -8,3 +8,9 @@ expect(person).to have_attributes(:name => a_string_starting_with("J"), :age => 
 expect(obj).to respond_to(:foo) # pass if obj.respond_to?(:foo)
 
 expect(obj.vertices).to match_array(array_without_order)
+
+RSpec.describe Template, type: :model do
+  it { is_expected.to have_fields(:name, :cs_user, :body, :subject, :with_logo).of_type(String) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:subject) }
