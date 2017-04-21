b0c639b111221993e90a1ad1ead0236bb100d1e2
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-04-20 20:09:12 -0700

    Update allow_spec.rb

--- allow_spec.rb
+++ allow_spec.rb
@@ -1,2 +1,9 @@
 
 find  spec -name *.rb|xargs grep allow|sed -e "s/.*allow/allow/"
+
+allow_any_instance_of(User).to receive(:name).and_return('name')
+allow(Settings).to receive(:exchange) { { enable: true } }
+allow(Settings).to receive(:exchange)and_return({ enable: true })
+allow(controller).to receive(:action_name) { 'show' }
+allow(controller).to receive(:controller_name) { 'global_settings' }
+allow(user).to receive(:full_name).with('xxx').and_return(true)
